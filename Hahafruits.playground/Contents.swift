272e1e710f548543441bd8105a3e760951c44966
Hans Lehman <hans.lehman@gmail.com>
2022-01-28 12:57:43 -0500

    Initial Commit

---
+++ Hahafruits.playground/Contents.swift
@@ -0,0 +1,16 @@
+// Single-line comments use two forward slashes (//)
+
+/*
+ Multi
+ Line
+ Comments
+ use a
+ slash
+ and an
+ asterisk
+ (/* comment here */)
+ */
+
+for _ in 1...5 {
+    print("haha" , terminator: " fruits\n")
+}
